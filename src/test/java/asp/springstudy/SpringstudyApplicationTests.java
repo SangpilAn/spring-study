0216cb97dd0df9867db65beda033a8e82eb21256
asp4303 <asp4303@gmail.com>
2023-01-03 11:25:13 +0900

    first commit

---
+++ src/test/java/asp/springstudy/SpringstudyApplicationTests.java
@@ -0,0 +1,13 @@
+package asp.springstudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringstudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
